b31da5f9c8044fc215a45d0f49eedf7493ad5114
adair <adair.l@qq.com>
2018-06-28 21:31:10 +0800

    Init
    I wanna keep this for further check

---
+++ CPPLessons/Lesson1_basic_type/Lesson1.h
@@ -0,0 +1,8 @@
+//
+// Created by Adair on 2018/6/28.
+//
+
+#ifndef LEARNING_LESSON1_H
+#define LEARNING_LESSON1_H
+
+#endif //LEARNING_LESSON1_H
---
+++ Lessons.h
@@ -0,0 +1,8 @@
+//
+// Created by Adair on 2018/6/28.
+//
+
+#ifndef LEARNING_LESSONS_H
+#define LEARNING_LESSONS_H
+
+#endif //LEARNING_LESSONS_H
